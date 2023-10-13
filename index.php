23f32ee8bc7acdb15e8f24dc2394332b815ff1d9
Angel320 <92888451+Angel320@users.noreply.github.com>
2023-10-12 18:31:49 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,310 @@
+<?php  
+require_once "cabecera.php";
+require_once "pie.php";
+?>
+
+            
+
+        <!-- PRINCIPAL-->
+        <header class="py-5 bg-image-full" style="background-image: url('https://www.westernunion.com/blog/wp-content/uploads/2022/03/Los_5_mejores_paises_para_visitar_en_Latinoamerica.jpg')">
+            <div data-aos="zoom-in" data-aos-duration="1800" class="text-center my-5">
+                <img class="img-fluid rounded-circle mb-4" src="https://cdn.pixabay.com/photo/2012/04/14/16/17/globe-34474_640.png" alt="..." width="180" height="180"/>
+                <h1 class="text-white fs-3 fw-bolder">Únete a esta comunidad</h1>
+                <p class="text-white-50 mb-0">Comparte - Viaja - Disfruta</p>
+            </div>
+        </header>
+        <!-- PRINCIPAL-->
+
+        <!-- PRIMER CONTENEDOR-->
+        <section class="py-5">
+
+            <div data-aos="fade-up" class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2>Se parte de esta comunidad Latinoamérica</h2>
+                        <p class="lead">Una comunidad para compartir.</p>
+                        <p class="mb-0">Únete a esta “Social media”  completamente Latinoamérica en la cual podrás compartir y visualizar fotos sobre paisajes, culturas, eventos y más en el día a día de varias personas que viven en diferentes países, en diferentes partes, diferentes barrios, ciudades, capitales, comunidades y demás que seguro tienen algo interesante que ofrecer.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- PRIMER CONTENEDOR-->
+
+       
+
+
+        <!-- CONTENEDOR CARUSEL-->
+        <section class="py-5">
+             <div data-aos="fade-up" class="container my-5">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                      <div class="carousel-indicators">
+                        
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                      </div>
+                      <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                          <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/c5/75/2a/photo1jpg.jpg?w=1200&h=-1&s=1" class="d-block w-100" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Viña del Mar - Chile</h5>
+                            <p>Viña del Mar es una ciudad turística costera al noroeste de Santiago de Chile. Es conocida por sus jardines, playas y altos edificios.</p>
+                          </div>
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="https://st1.uvnimg.com/4b/04/b4932e1c48bab1f5dea2b891bed1/gettyimages-176677797.jpg" class="d-block w-100" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Isla de Pascua - Chile</h5>
+                            <p>Isla de Pascua, un territorio de Chile, es una remota isla volcánica en la Polinesia.</p>
+                          </div>
+                        </div>
+
+                        <div class="carousel-item">
+                          <img src="https://imagenes.elpais.com/resizer/ZyTwa2eoE1xPZxhFMPauAviQVsQ=/1960x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/6AU7BXPLFPVVPIEUS55TGOTCLE.jpg" class="d-block w-100" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Machu Picchu - Perú</h5>
+                            <p>Machu Picchu es una ciudadela inca ubicada en las alturas de las montañas de los Andes en Perú.</p>
+                          </div>
+                        </div>
+                      </div>
+                      
+                      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
+                </div>
+             </div>    
+        </section>
+        <!-- CONTENEDOR CARUSEL-->
+
+
+        <!-- CONTENEDOR INTERMEDIO-->
+        <section class="py-5">
+            <div data-aos="fade-up" class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2>Interactúa con los demás</h2>
+                        <p class="lead">¡Adéntrate en otras culturas, tradiciones, festividades y demás!.</p>
+                        <p class="mb-0">Descubre lugares, eventos, noticias, tradiciones y demás de algún lugar de tu interés de entre toda Latinoamérica, marca tus preferencias, conoce a otras personas y descubre que a pesar de las diferencias tienen tanto en común.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- CONTENEDOR INTERMEDIO-->
+
+
+
+
+
+
+
+
+
+
+        <!-- PIE DE PAGINA-->
+        <footer class="py-5 bg-dark">
+            <!-- CONTACTO-->
+            <div data-aos="fade-right" class="social">
+                <ul> 
+                    <li><a href="https://www.facebook.com/UniversidadTecnologicadeCoahuila/?locale=es_LA"  id="icon-facebook" class="fa-brands fa-facebook-f" style="color: #ffffff;"></a></li>
+                    <li><a href="https://twitter.com/UTCoahuila?" id="icon-twitter" class="fa-brands fa-twitter" style="color: #ffffff;" ></a></li>
+                    <li><a href="https://www.instagram.com/utresculturas/?hl=es" id="icon-googleplus" class="fa-brands fa-instagram"></a></li>
+                    <li><a href="https://pin.it/42F65QG" id="icon-pinterest" class="fa-brands fa-pinterest-p" style="color: #ffffff;"></a></li>
+                    <li><a href="#" id="icon-mail" class="fa-sharp fa-regular fa-envelope" style="color: #ffffff;"></a></li>
+                </ul>
+            </div>
+            <!-- CONTACTO-->
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; LATAM-erica Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white"><a class="m-0 text-center text-white" href="opciones/mapa-del-sitio.php">Mapa del sitio</a></p></div>
+        </footer>
+        <!-- PIE DE PAGINA-->
+
+
+
+
+         <!-- LOGIN-->
+<div class="modal fade" id="Login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Inicio</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form  class="form-floating" name="form-ajax" id="form-login" method="post" action="#">
+
+
+                    <div class="mb-3">
+                        <label>Correo</label>
+                        <input class="form-control" type="text" placeholder="ejemplo@gmail.com" name="correolog" id="correolog">
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Contraseña</label>
+                        <input class="form-control" type="password" placeholder="Escribe una contraseña" name="pswlog" id="pswlog">
+                    </div>
+                    
+                    
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" >Cerrar</button>
+        <button type="button" class="btn btn-primary"  id="entrar">Entrar</button>
+
+        </form>
+
+
+      </div>
+    </div>
+  </div>
+</div>
+ <!-- LOGIN-->
+
+
+    </body>
+</html>
+
+
+
+
+
+<!-- ALERTAS DEL Inicio-->
+<script type="text/javascript">
+$(document).ready(function() {
+    let emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        var datos = $('#form-login').serialize();
+
+    $("#entrar").click(function() {
+    var datos = $('#form-login').serialize();
+    
+
+
+        //ESTRUCTURA DE ALERTA DEL CORREO
+        if ($("#correolog").val() == "") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 5000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+            Toast.fire({
+                icon: 'error',
+                //  Aqui pones el mensaje donde diga tittle
+                title: 'Escriba un correo, llene el campo'
+            })
+            return false;
+        } 
+        //ESTRUCTURA DE ALERTA DEL EMAIL
+
+
+        //ESTRUCTURA DE ALERTA DE LA CONTRASEÑA
+            else if ($("#pswlog").val() == "") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 5000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+            Toast.fire({
+                icon: 'error',
+                title: 'Escriba la contraseña, llene el campo'
+            })
+            return false;
+        }
+
+         
+
+        //ESTRUCTURA DEL AJAX
+                else  {
+                $.ajax({
+                    type:"POST",
+                    url:"BD/iniciar.php",
+                    data:datos,
+                    success:function(r){
+                         //R estaba tomando el mensaje de error
+                        if (r!=2) {
+                           // alert(r)
+
+                            //ALERTA DE EXITO, TIENE QUE HABER UNA PARA QUE TE LIMPIE EL FORMULARIO
+                                const Toast = Swal.mixin({
+                                    toast: true,
+                                    position: 'top-end',
+                                    showConfirmButton: false,
+                                    timer: 5000,
+                                    timerProgressBar: true,
+                                    didOpen: (toast) => {
+                                       // toast.addEventListener('mouseenter', Swal.stopTimer)
+                                        //toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                    }
+                                })
+                                Toast.fire({
+                                    icon: 'success',
+                                    title: 'Bienvenido usuario moderador'
+                                })
+
+                                                //$('#correo').val('');
+                                                //$('#psw').val('');
+                                                //$('#confirmar').val('');
+                                                //$('#pais').val('');
+
+
+
+                                                 setTimeout( function() { window.location.href = "opciones/administrador/Comentarios.php"; }, 5000 );
+
+
+
+
+
+
+                           
+                        }//CIERRE IF R DIFERENTE DE 2
+                                            else {
+                                                //Muestra el valor con 
+                                                alert(r)
+                                                const Toast = Swal.mixin({
+                                                toast: true,
+                                                position: 'top-end',
+                                                showConfirmButton: false,
+                                                timer: 5000,
+                                                timerProgressBar: true,
+                                                didOpen: (toast) => {
+                                                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                                                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                                }
+                                                 })
+                                                Toast.fire({
+                                                    icon: 'error',
+                                                    //  Aqui pones el mensaje donde diga tittle
+                                                    title: 'El correo o la contraseña no son validos'
+                                                })
+                                                //El swal detiene cualquier accion que haya por debajo de el
+                                                            swal ("¡Se Generó un Error Al Intentar Registrar!");
+
+                                            }
+                    }//CIERRE FUNCION R
+                });//CIERRE DEL ELSE DEL AJAX
+                return false;
+                }//CIERRE ELSE
+                
+
+    });
+
+});
+</script>
+<!-- ALERTAS DEL Inicio-->
