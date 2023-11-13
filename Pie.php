a2a9d83fc01cf27b76537616172cb229ad39e11f
AxelM05 <20045097@alumno.utc.edu.mx>
2023-11-13 12:08:17 -0600

    index

---
+++ Pie.php
@@ -0,0 +1,31 @@
+ <!-- PIE DE PAGINA-->
+        <footer class="py-5 bg-dark">
+            
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; CompuShop Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white"><a class="m-0 text-center text-white" href="#">Derechos reservados</a></p></div>
+        </footer>
+        <!-- PIE DE PAGINA-->
+
+
+         <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Captcha-->
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+        <!-- Libreria script de las animaciones-->
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <!-- Iniciador de las animaciones-->
+        <script>
+          AOS.init();
+        </script>
+
+        <!-- Script Cargador-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/custom.js"></script>
+        <!-- Script Cargador-->
+
+        <!-- Script Carrusel-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,186 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
-</html>
+ 
+<?php  
+require_once "Cabecera.php";
+?>
+
+
+
+     <!-- CONTENEDOR CARUSEL-->
+      <section  class="banner_main">
+         <div id="banner1" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#banner1" data-slide-to="0" class="active"></li>
+               <li data-target="#banner1" data-slide-to="1"></li>
+               <li data-target="#banner1" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <div class="text-bg">
+                           <h1> <span class="blu">Bienvenidos <br></span>Productos de Calidad</h1>
+                           <figure><img src="images/Producto1.png" alt="#"/></figure>
+                           <a class="read_more" href="#">Comprar Ahora</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <div class="text-bg">
+                           <h1> <span class="blu">Memoria 8GB 3200MT/s<br></span>Buena calidad</h1>
+                           <figure><img src="images/Producto2.png" alt="#"/></figure>
+                           <a class="read_more" href="#">Comprar Ahora</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <div class="text-bg">
+                           <h1> <span class="blu">Mando pro <br></span>Wireless Controller</h1>
+                           <figure><img src="images/Producto3.png" alt="#"/></figure>
+                           <a class="read_more" href="#">Comprar Ahora</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#banner1" role="button" data-slide="prev">
+            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+            </a>
+            <a class="carousel-control-next" href="#banner1" role="button" data-slide="next">
+            <i class="fa fa-arrow-right" aria-hidden="true"></i>
+            </a>
+         </div>
+      </section>
+      <!-- CONTENEDOR CARUSEL-->
+
+
+
+ <!-- Galeria de productos -->
+<section class="py-5">
+   <div class="container my-5">
+      <div class="row justify-content-center">
+
+
+
+      <div class="glasses">
+        
+
+         <div data-aos="fade-up" class="container">
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                  <div class="titlepage">
+                     <h2>Nuestros Productos</h2>
+                     <p>
+                     Productos calidad precio, los mejores precios del mercado, tecnologia de primera, originales.
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+
+         <div data-aos="fade-up" data-aos-duration="2400" class="container-fluid">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto1.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto2.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto3.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto4.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto5.jpg" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto6.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto7.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                  <div class="glasses_box">
+                     <figure><img src="images/Producto8.png" alt="#"/></figure>
+                     <h3><span class="blu">$</span>50</h3>
+                     <p>Sunglasses</p>
+                  </div>
+               </div>
+               <div class="col-md-12">
+                  <a class="read_more" href="#">Revisar Catalogo</a>
+               </div>
+            </div>
+         </div>
+
+
+
+      </div>
+
+      </div>
+   </div>
+</section>
+      <!-- Galeria de Productos -->
+
+      <!-- PRIMER CONTENEDOR-->
+        <section class="py-5">
+            <div data-aos="fade-up" class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h2>CompuShop</h2>
+                        <p class="lead">Objetivo general.</p>
+                        <p class="mb-0">Nuestro objetivo general es satisfacer de manera eficaz las necesidades del mercado, buscando la mejora continua en los servicios de la empresa.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+      <!-- PRIMER CONTENEDOR-->
+
+
+
+     
+       
+
+
+
+
+
+<?php  
+require_once "Pie.php";
+?>
+
