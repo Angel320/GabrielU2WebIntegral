0520991ba64033d0a41b9ffde02273638987842f
AxelM05 <20045097@alumno.utc.edu.mx>
2023-11-13 11:57:30 -0600

    Aplicacion Completa

---
+++ admin.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+       <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>CompuShop</title>
+        <!-- Favicon-->
+        <link rel="icon" href="https://exploralat.am/images/shadowlight.png" />
+
+      <!-- LIBRERIAS-->
+        <!-- Libreria estilos de la animación-->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <!-- Libreria para validar con JQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"> </script>
+        <!-- Libreria de sweetalert 2-->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <!-- Libreria para Iconos-->
+        <script src="https://kit.fontawesome.com/0727983454.js" crossorigin="anonymous"></script>
+      <!-- LIBRERIAS-->
+
+
+      <!-- ESTILOS-->
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+         <!-- Estilo de contactos-->
+        <link rel="stylesheet" href="css/contacto.css">
+        <!-- ESTILO FORMU-->
+        <link rel="stylesheet" href="css/estilos.css">
+
+
+        <!-- Estilo componente y carrusel -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Estilo de HOME -->
+      <link rel="stylesheet" href="css/home.css">
+      <!-- Estilo Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- ESTILOS-->
+
+
+      </head>
+
+<!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="index.html"><img src="https://cdn-icons-png.flaticon.com/512/2039/2039006.png" width="60" height="60" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item">
+                                 <a class="nav-link" href="agg_productos.php">Agregar Productos</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="web_service.php">RESTful</a>
+                              </li>              
+                           </ul>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>            
+ <!-- header -->
---
+++ restful.php
@@ -0,0 +1,42 @@
+<?php
+	include 'conexion.php';
+	
+	$pdo = new Conexion();
+	
+	//Listar registros y consultar registro
+	if($_SERVER['REQUEST_METHOD'] == 'GET'){
+		if(isset($_GET['id']))
+		{
+			$sql = $pdo->prepare("SELECT * FROM usuarios WHERE id=:id");
+			$sql->bindValue(':id', $_GET['id']);
+			$sql->execute();
+			$sql->setFetchMode(PDO::FETCH_ASSOC);
+			header("HTTP/1.1 200 hay datos");
+			echo json_encode($sql->fetchAll());
+			exit;				
+			
+			} else {
+			
+			$sql = $pdo->prepare("SELECT * FROM usuarios");
+			$sql->execute();
+			$sql->setFetchMode(PDO::FETCH_ASSOC);
+			header("HTTP/1.1 200 hay datos");
+			echo json_encode($sql->fetchAll());
+			exit;		
+		}
+	}
+	
+	//Eliminar registro
+	if($_SERVER['REQUEST_METHOD'] == 'DELETE')
+	{
+		$sql = "DELETE FROM usuarios WHERE id=:id";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(':id', $_GET['id']);
+		$stmt->execute();
+		header("HTTP/1.1 200 Ok");
+		exit;
+	}
+	
+	//Si no corresponde a ninguna opción anterior
+	header("HTTP/1.1 400 Bad Request");
+?>
